1b400ff3d63cae5125b3ed0bdace1642995b7e9b
Michael Matveev <michael.s.matveev@gmail.com>
2015-04-30 14:27:15 +0300

    Initial commit

---
+++ HubSender/Program.cs
@@ -0,0 +1,45 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Microsoft.ServiceBus.Messaging;
+
+namespace HubSender
+{
+    class Program
+    {
+        static string eventHubName = "counters-event-hub";
+        static string connectionString = "Endpoint=sb://cloud-counters.servicebus.windows.net/;SharedAccessKeyName=SendRule;SharedAccessKey=FXKG5DSuAxpFUf0AOLuO83d33LrBiCrQC3joP8X6mqI=";
+
+        static async Task SendingRandomMessages()
+        {
+            var eventHubClient = EventHubClient.CreateFromConnectionString(connectionString, eventHubName);
+            while (true)
+            {
+                try
+                {
+                    var message = Guid.NewGuid().ToString();
+                    Console.WriteLine("{0} > Sending message: {1}", DateTime.Now.ToString(), message);
+                    await eventHubClient.SendAsync(new EventData(Encoding.UTF8.GetBytes(message)));
+                }
+                catch (Exception exception)
+                {
+                    Console.ForegroundColor = ConsoleColor.Red;
+                    Console.WriteLine("{0} > Exception: {1}", DateTime.Now.ToString(), exception.Message);
+                    Console.ResetColor();
+                }
+
+                await Task.Delay(200);
+            }
+        }
+
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Press Ctrl-C to stop the sender process");
+            Console.WriteLine("Press Enter to start now");
+            Console.ReadLine();
+            SendingRandomMessages().Wait();
+        }
+    }
+}
